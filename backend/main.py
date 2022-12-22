c9f719abfcfc7f3c882db2fa7721d68a6b8b0402
lewiscabban <lcabban@gmail.com>
2022-12-22 21:48:04 +1030

    adding middleware

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,7 @@ import uvicorn
 import strawberry
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from strawberry.fastapi import GraphQLRouter
 
 from context import  BudgetTrackerManager
@@ -11,6 +12,13 @@ from querys import Query
 
 app = FastAPI()
 schema = strawberry.Schema(Query, Mutation)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.on_event("startup")
 async def startup_event() -> None:
