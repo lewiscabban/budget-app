8bad1d65e62b5e8bc8c707d89eb7772f6641b038
lewiscabban <lcabban@gmail.com>
2023-02-12 21:25:22 +1030

    adding GraphQL queries to frontend

--- frontend/budget-tracker/src/app/services/graphql.queries.ts
+++ frontend/budget-tracker/src/app/services/graphql.queries.ts
@@ -1,24 +1,325 @@
 import { gql } from "apollo-angular"
 
-const GET_USER = gql`
-    mutation {
-        getUser(userId: "chatooka") {
-            userId
-            incomeStreams {
-              amount
-              payDay
-              payCycle
-            }
-            budgets {
-              budgetId
-              amount
-              target
-              percentage
+const mutate_add_budget_tracker = gql`
+        mutation AddBudgetTracker(budgetTrackerId: String!) {
+            addBudgetTracker(budgetTrackerId: $budgetTrackerId) {
+                budgetTrackerName
+                incomes {
+                  incomeName
+                  amount
+                  payDay
+                  payRate
+                }
+                bills {
+                  billName
+                  amount
+                  payDay
+                  payRate
+                }
+                budgets {
+                  budgetName
+                  amount
+                  budgetType
+                }
             }
         }
-    }
-`
+    `
+
+const mutate_get_budget_tracker = gql`
+        mutation GetBudgetTracker(budgetTrackerId: String!) {
+            getBudgetTracker(budgetTrackerId: $budgetTrackerId) {
+                budgetTrackerName
+                incomes {
+                  incomeName
+                  amount
+                  payDay
+                  payRate
+                }
+                bills {
+                  billName
+                  amount
+                  payDay
+                  payRate
+                }
+                budgets {
+                  budgetName
+                  amount
+                  budgetType
+                }
+            }
+        }
+    `
+
+const mutate_add_budget = gql`
+        mutation AddBudget(budgetTrackerId: String!, budgetName: String!, amount: Float!, budgetType: String!) {
+            addBudget(budgetTrackerId: $budgetTrackerId, budgetName: $budgetName, amount: $amount, budgetType: $budgetType) {
+                budgetTrackerName
+                incomes {
+                  incomeName
+                  amount
+                  payDay
+                  payRate
+                }
+                bills {
+                  billName
+                  amount
+                  payDay
+                  payRate
+                }
+                budgets {
+                  budgetName
+                  amount
+                  budgetType
+                }
+            }
+        }
+    `
+
+const mutate_get_budget = gql`
+        mutation GetBudget(budgetTrackerId: String!, budgetName: String!!) {
+          getBudget(budgetTrackerId: $budgetTrackerId, budgetName: $budgetName) {
+                budgetName
+                amount
+                budgetType
+            }
+        }
+    `
+
+const mutate_update_budget = gql`
+        mutation UpdateBudget(budgetTrackerId: String!, oldBudgetName: String!, newBudgetName: String!, amount: Float!, budgetType: String!) {
+          updateBudget(budgetTrackerId: $budgetTrackerId, oldBudgetName: $oldBudgetName, newBudgetName: $newBudgetName, amount: $amount, budgetType: $budgetType) {
+                budgetTrackerName
+                incomes {
+                  incomeName
+                  amount
+                  payDay
+                  payRate
+                }
+                bills {
+                  billName
+                  amount
+                  payDay
+                  payRate
+                }
+                budgets {
+                  budgetName
+                  amount
+                  budgetType
+                }
+            }
+        }
+    `
+
+const mutate_delete_budget = gql`
+        mutation DeleteBudget(budgetTrackerId: String!, budgetName: String!) {
+          deleteBudget(budgetTrackerId: $budgetTrackerId, budgetName: $budgetName) {
+                budgetTrackerName
+                incomes {
+                  incomeName
+                  amount
+                  payDay
+                  payRate
+                }
+                bills {
+                  billName
+                  amount
+                  payDay
+                  payRate
+                }
+                budgets {
+                  budgetName
+                  amount
+                  budgetType
+                }
+            }
+        }
+    `
+
+const mutate_add_income = gql`
+        mutation AddIncome(budgetTrackerId: String!, incomeName: String!, amount: Float!, payDay: String!, payRate: String!) {
+            addIncome(budgetTrackerId: $budgetTrackerId, incomeName: $incomeName, amount: $amount, payDay: $payDay, payRate: $payRate) {
+                budgetTrackerName
+                incomes {
+                  incomeName
+                  amount
+                  payDay
+                  payRate
+                }
+                bills {
+                  billName
+                  amount
+                  payDay
+                  payRate
+                }
+                budgets {
+                  budgetName
+                  amount
+                  budgetType
+                }
+            }
+        }
+    `
+
+const mutate_get_income = gql`
+        mutation GetIncome(budgetTrackerId: String!, incomeName: String!) {
+          getIncome(budgetTrackerId: $budgetTrackerId, incomeName: $incomeName) {
+                incomeName
+                pay_rate
+                pay_day
+                amount
+            }
+        }
+    `
+
+const mutate_update_income = gql`
+        mutation UpdateIncome(budgetTrackerId: String!, oldIncomeName: String!, newIncomeName: String!, amount: Float!, payDay: String!, payRate: String!) {
+          updateIncome(budgetTrackerId: $budgetTrackerId, oldIncomeName: $oldIncomeName, newIncomeName: $newIncomeName, amount: $amount, payDay: $payDay, payRate: $payRate) {
+                budgetTrackerName
+                incomes {
+                  incomeName
+                  amount
+                  payDay
+                  payRate
+                }
+                bills {
+                  billName
+                  amount
+                  payDay
+                  payRate
+                }
+                budgets {
+                  budgetName
+                  amount
+                  budgetType
+                }
+            }
+        }
+    `
+
+const mutate_delete_income = gql`
+        mutation DeleteIncome(budgetTrackerId: String!, incomeName: String!) {
+          deleteIncome(budgetTrackerId: $budgetTrackerId, incomeName: $incomeName) {
+                budgetTrackerName
+                incomes {
+                  incomeName
+                  amount
+                  payDay
+                  payRate
+                }
+                bills {
+                  billName
+                  amount
+                  payDay
+                  payRate
+                }
+                budgets {
+                  budgetName
+                  amount
+                  budgetType
+                }
+            }
+        }
+    `
+
+const mutate_add_bill = gql`
+        mutation AddBill(budgetTrackerId: String!, billName: String!, amount: Float!, payDay: String!, payRate: String!) {
+            addBill(budgetTrackerId: $budgetTrackerId, billName: $billName, amount: $amount, payDay: $payDay, payRate: $payRate) {
+                budgetTrackerName
+                incomes {
+                  incomeName
+                  amount
+                  payDay
+                  payRate
+                }
+                bills {
+                  billName
+                  amount
+                  payDay
+                  payRate
+                }
+                budgets {
+                  budgetName
+                  amount
+                  budgetType
+                }
+            }
+        }
+    `
+
+const mutate_get_bill = gql`
+        mutation GetBill(budgetTrackerId: String!, billName: String!) {
+          getBill(budgetTrackerId: $budgetTrackerId, billName: $billName) {
+                billName
+                pay_rate
+                pay_day
+                amount
+            }
+        }
+    `
+
+const mutate_update_bill = gql`
+        mutation UpdateBill(budgetTrackerId: String!, oldBillName: String!, newBillName: String!, amount: Float!, payDay: String!, payRate: String!) {
+          updateBill(budgetTrackerId: $budgetTrackerId, oldBillName: $oldBillName, newBillName: $newBillName, amount: $amount, payDay: $payDay, payRate: $payRate) {
+                budgetTrackerName
+                incomes {
+                  incomeName
+                  amount
+                  payDay
+                  payRate
+                }
+                bills {
+                  billName
+                  amount
+                  payDay
+                  payRate
+                }
+                budgets {
+                  budgetName
+                  amount
+                  budgetType
+                }
+            }
+        }
+    `
+
+const mutate_delete_bill = gql`
+        mutation DeleteBill(budgetTrackerId: String!, billName: String!) {
+          deleteBill(budgetTrackerId: $budgetTrackerId, billName: $billName) {
+                budgetTrackerName
+                incomes {
+                  incomeName
+                  amount
+                  payDay
+                  payRate
+                }
+                bills {
+                  billName
+                  amount
+                  payDay
+                  payRate
+                }
+                budgets {
+                  budgetName
+                  amount
+                  budgetType
+                }
+            }
+        }
+    `
 
 export {
-    GET_USER,
+  mutate_add_budget_tracker,
+  mutate_get_budget_tracker,
+  mutate_add_budget,
+  mutate_get_budget,
+  mutate_update_budget,
+  mutate_delete_budget,
+  mutate_add_income,
+  mutate_get_income,
+  mutate_update_income,
+  mutate_delete_income,
+  mutate_add_bill,
+  mutate_get_bill,
+  mutate_update_bill,
+  mutate_delete_bill,
 }
--- frontend/budget-tracker/src/app/services/graphql.service.ts
+++ frontend/budget-tracker/src/app/services/graphql.service.ts
@@ -1,7 +1,22 @@
 import { Injectable } from "@angular/core";
 import { Apollo } from "apollo-angular";
 
-import { GET_USER } from "./graphql.queries";
+import { 
+    mutate_add_budget_tracker,
+    mutate_get_budget_tracker,
+    mutate_add_budget,
+    mutate_get_budget,
+    mutate_update_budget,
+    mutate_delete_budget,
+    mutate_add_income,
+    mutate_get_income,
+    mutate_update_income,
+    mutate_delete_income,
+    mutate_add_bill,
+    mutate_get_bill,
+    mutate_update_bill,
+    mutate_delete_bill, 
+} from "./graphql.queries";
 
 @Injectable({
     providedIn: "root",
@@ -9,9 +24,157 @@ import { GET_USER } from "./graphql.queries";
 export class GraphQLService {
     constructor(private apollo: Apollo) {}
 
-    public getUser() {
+    public add_budget_tracker(budgetTrackerId: String) {
         return this.apollo.mutate({
-            mutation: GET_USER,
+            mutation: mutate_add_budget_tracker,
+            variables: {
+                budgetTrackerId: budgetTrackerId,
+            }
+        })
+    }
+
+    public get_budget_tracker(budgetTrackerId: String) {
+        return this.apollo.mutate({
+            mutation: mutate_get_budget_tracker,
+            variables: {
+                budgetTrackerId: budgetTrackerId,
+            }
+        })
+    }
+
+    public add_budget(budgetTrackerId: String, budgetName: String, amount: Number, budgetType: String) {
+        return this.apollo.mutate({
+            mutation: mutate_add_budget,
+            variables: {
+                budgetTrackerId: budgetTrackerId,
+                budgetName: budgetName,
+                amount: amount,
+                budgetType: budgetType,
+            }
+        })
+    }
+
+    public get_budget(budgetTrackerId: String, budgetName: String) {
+        return this.apollo.mutate({
+            mutation: mutate_get_budget,
+            variables: {
+                budgetTrackerId: budgetTrackerId,
+                budgetName: budgetName,
+            }
+        })
+    }
+
+    public update_budget(budgetTrackerId: String, budgetName: String, amount: Number, budgetType: String) {
+        return this.apollo.mutate({
+            mutation: mutate_update_budget,
+            variables: {
+                budgetTrackerId: budgetTrackerId,
+                budgetName: budgetName,
+                amount: amount,
+                budgetType: budgetType,
+            }
+        })
+    }
+
+    public delete_budget(budgetTrackerId: String, budgetName: String) {
+        return this.apollo.mutate({
+            mutation: mutate_delete_budget,
+            variables: {
+                budgetTrackerId: budgetTrackerId,
+                budgetName: budgetName,
+            }
+        })
+    }
+
+    public add_income(budgetTrackerId: String, incomeName: String, amount: Number, payDay: String, payRate: String) {
+        return this.apollo.mutate({
+            mutation: mutate_add_income,
+            variables: {
+                budgetTrackerId: budgetTrackerId,
+                incomeName: incomeName,
+                amount: amount,
+                payDay: payDay,
+                payRate: payRate,
+            }
+        })
+    }
+
+    public get_income(budgetTrackerId: String, incomeName: String) {
+        return this.apollo.mutate({
+            mutation: mutate_get_income,
+            variables: {
+                budgetTrackerId: budgetTrackerId,
+                incomeName: incomeName,
+            }
+        })
+    }
+
+    public update_income(budgetTrackerId: String, incomeName: String, amount: Number, payDay: String, payRate: String) {
+        return this.apollo.mutate({
+            mutation: mutate_update_income,
+            variables: {
+                budgetTrackerId: budgetTrackerId,
+                incomeName: incomeName,
+                amount: amount,
+                payDay: payDay,
+                payRate: payRate,
+            }
+        })
+    }
+
+    public delete_income(budgetTrackerId: String, incomeName: String) {
+        return this.apollo.mutate({
+            mutation: mutate_delete_income,
+            variables: {
+                budgetTrackerId: budgetTrackerId,
+                incomeName: incomeName,
+            }
+        })
+    }
+
+    public add_bill(budgetTrackerId: String, billName: String, amount: Number, payDay: String, payRate: String) {
+        return this.apollo.mutate({
+            mutation: mutate_add_bill,
+            variables: {
+                budgetTrackerId: budgetTrackerId,
+                billName: billName,
+                amount: amount,
+                payDay: payDay,
+                payRate: payRate,
+            }
+        })
+    }
+
+    public get_bill(budgetTrackerId: String, billName: String) {
+        return this.apollo.mutate({
+            mutation: mutate_get_bill,
+            variables: {
+                budgetTrackerId: budgetTrackerId,
+                billName: billName,
+            }
+        })
+    }
+
+    public update_bill(budgetTrackerId: String, billName: String, amount: Number, payDay: String, payRate: String) {
+        return this.apollo.mutate({
+            mutation: mutate_update_bill,
+            variables: {
+                budgetTrackerId: budgetTrackerId,
+                billName: billName,
+                amount: amount,
+                payDay: payDay,
+                payRate: payRate,
+            }
+        })
+    }
+
+    public delete_bill(budgetTrackerId: String, billName: String) {
+        return this.apollo.mutate({
+            mutation: mutate_delete_bill,
+            variables: {
+                budgetTrackerId: budgetTrackerId,
+                billName: billName,
+            }
         })
     }
 }
